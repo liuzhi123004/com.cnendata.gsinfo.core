3e4184458e2d4202248495a8e04b1c19b82c34e4
enilu <82552623@qq.com>
2014-12-29 09:26:55 +0800

    初始化

---
+++ src/com/cnendata/gsinfo/core/TestApi.java
@@ -0,0 +1,27 @@
+/**
+ *TestApi.java
+ *Version1.0
+ *2014-12-28
+ *Copyright cnendata.com
+ *
+ */
+package com.cnendata.gsinfo.core;
+
+/**
+ * descript<br>
+ * <!--<br>
+ * 历史记录：<br>
+ * --------------------------------------------------------
+ * 2014-12-28,enilu(82552623@qq.com)新建文档<br>
+ * 
+ * -->
+ * 
+ * @author enilu(82552623@qq.com)
+ * 
+ *         since1.0
+ */
+public class TestApi {
+	public String getVal() {
+		return "hello,测试api";
+	}
+}
